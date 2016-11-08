4dbd782284ad121e372b4c5af1f2e48b9b855004
dworthen <worthend.derek@gmail.com>
2016-11-07 17:27:49 -0800

    Scaffold project structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// require("../node_modules/font-awesome/css/font-awesome.min.css");
+// require("../styles/group.css");
+var Elm = require("./Main.elm");
+
+var app = Elm.Main.embed(document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+var argv = require('yargs').argv;
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  devtool: (argv.inline || argv.hot) ? "#eval" : "#source-map",
+  output: {
+    filename: './scripts/index.js'
+  },
+  plugins: [
+      new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.elm$/,
+        exclude: [/node_modules/, /elm-stuff/],
+        loader: 'elm-hot!elm-webpack',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css' 
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url'
+      },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?mimetype=application/font-woff"
+      }, 
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?mimetype=application/font-woff"
+      }, 
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?mimetype=application/octet-stream"
+      }, 
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?mimetype=application/vnd.ms-fontobject"
+      }, 
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?mimetype=image/svg+xml"
+      }
+    ],
+    noParse: /\.elm$/
+  }
+};
